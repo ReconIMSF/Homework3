77ed82201ece9d0d7720e5a969fb105bd437ecc2
ReconIMSF <dorbatorg@gmail.com>
2024-10-14 22:59:25 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Задание 1
+        int a = 100000;
+        byte b = 66;
+        short c = 32000;
+
+
+
+        int, byte, short, long, float, double
+    }
+}
